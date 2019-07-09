9214526cbcaeebe8d7f96e508f4101410a7fc6b1
Abraham Farias <afarias@Abrahams-MacBook-Air.local>
2019-07-09 14:12:44 -0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recreate-form';
+
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    
+  }
 }
